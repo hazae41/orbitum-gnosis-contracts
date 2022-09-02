544cd1395f419c6f191c7c46bcda036027309ecb
hazae41 <hazae41@proton.me>
2022-09-03 00:45:25 +0200

    author

--- gateway.sol
+++ gateway.sol
@@ -1,57 +1,56 @@
-pragma solidity >= 0.8.7;
+pragma solidity >=0.8.7;
 
 // SPDX-License-Identifier: MIT
 
 contract Gateway {
-  address relayer;
+    address relayer;
 
-  constructor() {
-    relayer = msg.sender;
-  }
+    constructor() {
+        relayer = msg.sender;
+    }
 
-  function transfer(
-    address relayer2
-  ) public {
-    require(msg.sender == relayer, "Not relayer");
+    function transfer(address relayer2) public {
+        require(msg.sender == relayer, "Not relayer");
 
-    relayer = relayer2;
-  }
+        relayer = relayer2;
+    }
 
-  event Topic(
-    string indexed id,
-    string indexed forum,
-    address indexed author,
-    string title,
-    string text
-  );
+    event Topic(
+        uint256 indexed id,
+        address indexed author,
+        string forum,
+        string title
+    );
 
-  function create(
-    string calldata id,
-    string calldata forum,
-    address author,
-    string calldata title,
-    string memory text
-  ) public {
-    require(msg.sender == relayer, "Not relayer");
+    function create(
+        uint256 topic,
+        uint256 post,
+        address author,
+        string calldata forum,
+        string calldata title,
+        string memory text
+    ) public {
+        require(msg.sender == relayer, "Not relayer");
 
-    emit Topic(id, forum, author, title, text);
-  }
+        emit Topic(topic, author, forum, title);
+        emit Post(post, topic, author, text);
+    }
 
-  event Post(
-    string indexed id,
-    string indexed topic, 
-    address indexed author, 
-    string text
-  );
+    event Post(
+        uint256 indexed id,
+        uint256 indexed topic,
+        address indexed author,
+        string text
+    );
 
-  function reply(
-    string calldata id,
-    string calldata topic, 
-    address author,
-    string memory text
-  ) public {
-    require(msg.sender == relayer, "Not relayer");
-    
-    emit Post(id, topic, author, text);
-  }
+    function reply(
+        uint256 id,
+        uint256 topic,
+        address author,
+        string memory text
+    ) public {
+        require(msg.sender == relayer, "Not relayer");
+
+        emit Post(id, topic, author, text);
+    }
 }

a07908590dea6909fdd9df0e69d438dc2cd15db3
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-10-26 11:57:01 +0200

    tx hash

--- donations.sol
+++ donations.sol
@@ -7,17 +7,19 @@ import "./relayable.sol";
 contract Donations is Relayable {
 
     event Donated(
+        string txHash,
         uint256 indexed post,
         address indexed donator,
         uint256 amount
     );
 
     function donate(
+      string calldata txHash,
       address donator,
       uint256 post,
       uint256 amount
     ) public onlyRelayer {
-      emit Donated(post, donator, amount);
+      emit Donated(txHash, post, donator, amount);
     }
 
 }
